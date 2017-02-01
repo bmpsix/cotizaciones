8f12603be4029e9d0edae27ddf03ef5bad94bffc
kiramz7 <kiramz7@kiramz7-PC>
2017-01-31 23:39:48 -0600

    Consecutivos

--- src/main/java/com/unimer/cotizaciones/entities/Consecutive.java
+++ src/main/java/com/unimer/cotizaciones/entities/Consecutive.java
@@ -14,6 +14,7 @@ public class Consecutive implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue
 	@Column(name="id_consecutive", unique=true, nullable=false)
 	private int idConsecutive;
 
--- src/main/java/com/unimer/cotizaciones/repositories/ConsecutivesJpaRepository.java
+++ src/main/java/com/unimer/cotizaciones/repositories/ConsecutivesJpaRepository.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;
 
+
 import com.unimer.cotizaciones.entities.Consecutive;
 
 @Repository("consecutivesJpaRepository")
